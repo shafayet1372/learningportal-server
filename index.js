85ba4137cb1575926010b9f584a5393a58a43ee6
Shafayet Hossain <shafayethossain777@gmail.com>
2023-04-10 01:17:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const jsonServer = require("json-server"); // importing json-server library
+const auth=require("json-server-auth");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const middlewares = auth.defaults();
 const port = 9000; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
